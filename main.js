2e65fe89f56cedc39b4b97819c83f9f3ac0f278d
alexkir2304 <47975346+alexkir2304@users.noreply.github.com>
2021-06-23 21:40:23 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+let modal= document.querySelectorAll('.modal'); //модальное окно целиком
+let img = document.querySelectorAll('.myImg'); //кликабельная картинка из галереи
+let modalImg = document.querySelectorAll('.modalImg'); //картинка модального окна
+
+for (let i = 0; i < img.length; i++) {
+    img[i].onclick = function(){
+        modal[0].style.display = "block";
+        modalImg[0].src = this.src;
+    }
+}
+// modal[0].addEventListener('click', function(event) {
+//     event.target.style.opacity = '0.5';
+// })
+
+
+modal[0].addEventListener('click', function(event) {
+    if (event.target !== modalImg[0])  modal[0].style.display = "none";
+})
+
+
+
+
+
+
+
+
+
